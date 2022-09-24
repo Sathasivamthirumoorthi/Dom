eede9dd2a8405f2cc659b4d3422c023500351fb1
Sathasivamthirumoorthi <sathasivamthirumoorthi@gmail.com>
2022-09-24 22:16:09 +0530

    first

---
+++ index.js
@@ -0,0 +1,205 @@
+// const h1 = document.getElementById('text')
+
+// console.log(h1.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode)
+
+
+// const h1 = document.getElementsByTagName('h1')
+
+// const h1_array = Array.from(h1)
+
+// console.log(h1_array)
+
+
+// h1_array.forEach(element =>{
+//     element.setAttribute('style',`color : ${'red'}`)
+// })
+
+
+
+// const body = document.body
+// const div = document.createElement('div')
+
+
+// div.innerHTML = "<h1>Hello world .....</h1>"
+// div.innerHTML = "Hello world"
+
+// body.append(div)
+
+// const text = document.getElementById('text')
+
+// text.textContent = "Hello......"
+
+// console.log(text.textContent)
+// console.log(text.innerText)
+
+
+
+// console.log(text)
+
+// const div = document.createElement('div')
+
+// const h1 = document.createElement('h1')
+
+
+// div.textContent = "Hello world"
+
+// div.innerText = "Hello mars"
+
+// body.append(div)
+
+
+
+// body.append(h1)
+// body.appendChild("Hello world")
+
+
+// console.log(h1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// getElementByID 
+// getElementByClassName 
+// getElementByTagName
+// querySelector
+// querySelectorAll
+// Array.from -> converts obj into array
+// forEach 
+
+
+
+
+
+
+
+
+
+
+// const child=document.getElementsByClassName('child')
+// const child_array=Array.from(child)
+// child_array.forEach((element)=>{
+
+//     return element.style.backgroundColor = 'blue'
+    
+//     // console.log(child)
+// })
+    
+
+
+
+// document.body.style.backgroundColor
+
+
+// const h1 = document.getElementsByTagName('h1')
+// console.log(h1)
+
+// const h1_array = Array.from(h1)
+
+// function changeBackGround(element){
+//     return element.style.ba\ = 'blue'
+// }
+
+// h1_array.forEach(element =>{
+//     changeBackGround(element)
+// })
+
+
+// const grand = document.querySelector('#gp')
+
+// document.ge
+
+
+// const parent = document.querySelectorAll('#parent')
+
+// console.log(parent)
+
+// console.log(typeof(parent))
+
+
+// const parent_array = Array.from(parent)
+// console.log(parent_array)
+
+// parent_array.forEach((element) =>{
+//     changeBackGround(element)
+// })
+
+
+
+
+// changeBackGround(parent)
+
+
+// console.log(grand)
+
+
+
+
+
+
+// const grand = document.getElementById('gp')
+
+// const parent = document.getElementById('parent')
+
+// const parent = document.getElementsByClassName('parent')
+
+// console.log(typeof(parent))
+
+// const parent_array = Array.from(parent)
+
+
+
+
+// parent_array.forEach((element) =>{
+//     changeBackGround(element)
+// })
+
+
+
+// console.log(parent_array)
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(parent_array )
+
+// for(let i = 0)
+
+// parent_array.forEach((element) =>{
+//     changeBackGround(element)
+// })
+
+
+// const sum =(a,b)=>{
+//     return a + b;
+// }
+
+
+// const sum = (a,b) =>{
+//     return a + b
+// }
+
+// console.log(sum(1,2))
+
+
+
+
+
